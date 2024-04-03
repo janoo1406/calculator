cc110a6af8b0e5f6daa19c4c69139a8e899afa2d
janoo1406 <jan.pastor6@gmail.com>
2024-04-03 07:32:25 +0200

    Calculator - styling and logic enhancements

--- public/client.js
+++ public/client.js
@@ -1,5 +1,106 @@
-const collectButton = document.getElementById('sample-button');
-collectButton.addEventListener('click', async (event) => {
-  let txtField = document.getElementById("sample-text");
-  txtField.value = "Updated content!";
+
+const zeroButton = document.getElementById('button-0');
+zeroButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "0";
 });
+
+const oneButton = document.getElementById('button-1');
+oneButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "1";
+});
+const twoButton = document.getElementById('button-2');
+twoButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "2";
+});
+const threeButton = document.getElementById('button-3');
+threeButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "3";
+});
+const fourButton = document.getElementById('button-4');
+fourButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "4";
+});
+const fiveButton = document.getElementById('button-5');
+fiveButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "5";
+});
+const sixButton = document.getElementById('button-6');
+sixButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "6";
+});
+const sevenButton = document.getElementById('button-7');
+sevenButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "7";
+});
+const eightButton = document.getElementById('button-8');
+eightButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "8";
+});
+const nineButton = document.getElementById('button-9');
+nineButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "9";
+});
+const plusButton = document.getElementById('button-+');
+plusButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "+";
+});
+const minusButton = document.getElementById('button--');
+minusButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "-";
+});
+const dotButton = document.getElementById('button-.');
+dotButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += ".";
+});
+const clearButton = document.getElementById('button-');
+clearButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value = "";
+});
+const mnozenieButton = document.getElementById('button-*');
+mnozenieButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "*";
+});
+const dzielenieButton = document.getElementById('button-/');
+dzielenieButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+  display.value += "/";
+});
+
+const sumaButton = document.getElementById('button-=');
+sumaButton.addEventListener('click', async (event) => {
+  let display = document.getElementById("display");
+     let input = ["*", "/", "+", "-"];
+      let changes = display.value;
+      let newData = "";
+    
+      for (let i = 0; i < changes.length; i++) {
+        if (input.includes(changes[i])) {
+            if (changes[i] !== newData[newData.length - 1]) {
+                newData += changes[i];
+            }
+        }
+        else {
+            newData += changes[i];
+        }
+    }
+    
+    console.log(newData);
+    display.value = eval(newData);
+      
+  
+  });
